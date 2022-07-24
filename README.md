ecc30cf6a10103511b199abb1bddadb94b0f3af2
ruchan <ruchan.yavuzdemir@icloud.com>
2022-07-24 21:29:21 +0300

    bug fixes

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Weather-CLI-Tool
 
-Hello, World!
-Finally!
+A Weather CLI tool gives you weather information if you type latitude and longitude values. For example, type minus for South Pole's latitude or GMT minus places' longitude.
+
+1. make sure you installed golang
+2. open linux/macos terminal
+3. go build -o awesome-weather-tool main.go
+4. ./awesome-weather-tool
+
--- main.go
+++ main.go
@@ -1,24 +1,11 @@
-// I/O part has been done, now fix the api http call part.
-
-// execution
-// go build -o awesome-weather-tool main.go
-// ./awesome-weather-tool
-
-// TO DO LIST
-
-// do string manipulation, change the values in the string
-// CLI tool makes an HTTP call to retrieve the info
-// implement structure, map sth. like that
-// parse the json output
-// http headers, request
-// Sprint function
-
 package main
 
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -28,39 +15,35 @@ func main() {
 	fmt.Scanf("%f", &latitude)
 	fmt.Printf("Longtitude? ")
 	fmt.Scanf("%f", &longtitude)
-	//var URL string = fmt.Sprint("https://api.openweathermap.org/data/3.0/onecall?lat=", latitude, "&lon=", longtitude, "&appid=f90f2252ffbb060ba8d8c3bd7e7e500d", "&units=metric")
+	var URL string = fmt.Sprint("https://api.openweathermap.org/data/3.0/onecall?lat=", latitude, "&lon=", longtitude, "&appid=f90f2252ffbb060ba8d8c3bd7e7e500d", "&units=metric")
 
-	var toyBody string = `{"temp":25, "humidity":50, "feels_like":30}`
-
-	/*response, err := http.Get(URL)
+	response, err := http.Get(URL)
 	if err != nil {
 		log.Fatalln(err)
-	}*/
+	}
 
-	/*body, err := ioutil.ReadAll(response.Body)
+	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatalln(err)
-	}*/
+	}
 
 	type Current struct {
 		Temp       float64 `json:"temp"`
 		Feels_like float64 `json:"feels_like"`
 		Humidity   float64 `json:"humidity"`
 	}
-	var result Current
-	//body_string := string(body)
-	err := json.Unmarshal([]byte(toyBody), &result)
+
+	type Weather struct {
+		Current Current `json:"current"`
+	}
+
+	var result Weather
+	text := string(body)
+	err = json.Unmarshal([]byte(text), &result)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	fmt.Printf("current temp: %v, ", result.Temp)
-	fmt.Printf("feels like: %v, ", result.Feels_like)
-	fmt.Printf("humidity: %v\n", result.Humidity)
-	fmt.Println(result)
+	fmt.Printf("current temp: %v, ", result.Current.Temp)
+	fmt.Printf("feels like: %v, ", result.Current.Feels_like)
+	fmt.Printf("humidity: %v\n", result.Current.Humidity)
 }
-
-/*
-	parsing JSON format
-	json.Marshall
-*/
